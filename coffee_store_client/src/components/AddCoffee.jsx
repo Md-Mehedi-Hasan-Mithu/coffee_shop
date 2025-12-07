8366dd3157202c48eaf05719e87124da2fe708d2
Mehedi Hasan <mdmehedi3799@gmail.com>
2025-12-07 11:48:13 +0600

    data uploaded in db

--- coffee_store_client/src/components/AddCoffee.jsx
+++ coffee_store_client/src/components/AddCoffee.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 const AddCoffee = () => {
+    const handleAddCoffee = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const coffeeData = Object.fromEntries(formData.entries());
+        console.log(coffeeData);
+
+        //send coffee  data to server
+        fetch('http://localhost:3000/coffees',{
+            method:"POST",
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(coffeeData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('After adding coffee to db',data)
+            })
+
+     }
   return (
     <div className='p-24'>
         <div className='p-12 text-center space-y-4'>
@@ -7,7 +28,7 @@ const AddCoffee = () => {
             <p>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. 
             The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
         </div>
-        <form >
+        <form onSubmit={handleAddCoffee}>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
                 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
                     <label className="label">Name</label>
